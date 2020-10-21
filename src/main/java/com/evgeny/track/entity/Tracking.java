e73ca5d923d253c1f8ea3aad45ad75825e27796f
Evgenygroup <mailfrom61@gmail.com>
2020-10-21 13:26:15 +0200

    changing tracking table name to lower case

--- src/main/java/com/evgeny/track/entity/Tracking.java
+++ src/main/java/com/evgeny/track/entity/Tracking.java
@@ -14,6 +14,7 @@ import java.util.Date;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "tracking")
 public class Tracking {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
