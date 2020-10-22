ccb9161e72324bc34f01faa8addb911c682cc6cb
Evgenygroup <mailfrom61@gmail.com>
2020-10-22 10:21:31 +0200

    changing methods to controllers

--- src/main/java/com/evgeny/track/controller/TrackingController.java
+++ src/main/java/com/evgeny/track/controller/TrackingController.java
@@ -5,22 +5,36 @@ import com.evgeny.track.entity.Tracking;
 import com.evgeny.track.service.TrackingService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
 import java.util.List;
 
 @RestController
 public class TrackingController {
-
     private TrackingService service;
+    private ModelMapper modelMapper;
 
     @Autowired
     public TrackingController(TrackingService service, ModelMapper modelMapper) {
         this.service = service;
+        this.modelMapper = modelMapper;
+    }
+
+    @GetMapping("/api/shipment/{id}")
+    public List<Tracking> getTrackingsById(@PathVariable("id") int shipmentId ) {
+        List<Tracking> trackingList = service.getTrackingsByShipmentId(shipmentId);
+
+        return trackingList;
+    }
+
+    @PostMapping("/api/shipments/{id}/trackings")
+    TrackingDTO addTracking(@RequestBody TrackingDTO tracking, @PathVariable long id) {
+
+        Date date = new Date();
+        Tracking trackingEntity = new Tracking(tracking.getTrackingId(),tracking.getStatus(), id, date);
+
+        return modelMapper.map(service.addTracking(trackingEntity), TrackingDTO.class);
     }
 
     @GetMapping("/api/tracking")
