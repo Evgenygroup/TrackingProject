8da9b51d2d5914330d0dc6cfa3d6089b85e17255
Evgenygroup <mailfrom61@gmail.com>
2020-10-27 10:18:54 +0100

    change tracking api back&front

--- src/main/java/com/evgeny/track/controller/TrackingController.java
+++ src/main/java/com/evgeny/track/controller/TrackingController.java
@@ -1,8 +1,6 @@
 package com.evgeny.track.controller;
 
-import com.evgeny.track.dto.ShipmentDTO;
 import com.evgeny.track.dto.TrackingDTO;
-import com.evgeny.track.entity.ShipmentEntity;
 import com.evgeny.track.entity.TrackingEntity;
 import com.evgeny.track.service.TrackingService;
 import org.modelmapper.ModelMapper;
@@ -16,41 +14,29 @@ import java.util.stream.Collectors;
 @RestController
 public class TrackingController {
     private TrackingService service;
-    private ModelMapper modelMapper;
+
 
     @Autowired
     public TrackingController(TrackingService service, ModelMapper modelMapper) {
         this.service = service;
-        this.modelMapper = modelMapper;
     }
 
-    @GetMapping("/api/trackings/{shipmentId}/trackings")
 
-    public List<TrackingDTO> getTrackingsByShipmentId(@PathVariable("shipmentId") long shipmentId ) {
-        return  service.getTrackingsByShipmentId(shipmentId)
+    @GetMapping("/api/trackings/{shipmentId}/trackings")
+    public List<TrackingDTO> getTrackingsByShipmentId(@PathVariable("shipmentId") long shipmentId) {
+        return service.getTrackingsByShipmentId(shipmentId)
                 .stream()
                 .map(this::convertToDTO)
                 .collect(Collectors.toList());
     }
 
 
-    @PostMapping("/api/trackings/{shipmentId}/trackings")
-    TrackingDTO addTrackingByShipmentId(@RequestBody TrackingDTO tracking, @PathVariable long shipmentId) {
-
-        Date date = new Date();
-        TrackingEntity trackingEntity = new TrackingEntity(tracking.getTrackingId(),tracking.getStatus(), shipmentId, date);
-
-        return modelMapper.map(service.addTracking(trackingEntity), TrackingDTO.class);
-    }
-
     @PostMapping("/api/tracking")
-    public TrackingEntity addTracking(@RequestBody TrackingDTO tracking) {
-
-        Date date = new Date();
-        TrackingEntity dbTracking = new TrackingEntity(null, tracking.getStatus(), tracking.getShipmentId(), date);
-
-        return service.addTracking(dbTracking);
+    public TrackingDTO addTracking(@RequestBody TrackingDTO trackingDTO) {
+        TrackingEntity tracking = convertToEntity(trackingDTO);
+        return convertToDTO(service.addTracking(tracking));
     }
+
 
     private TrackingDTO convertToDTO(TrackingEntity tracking) {
         TrackingDTO trackingDto = new TrackingDTO();
@@ -59,7 +45,17 @@ public class TrackingController {
         trackingDto.setShipmentId(tracking.getShipmentId());
         trackingDto.setEventDate(tracking.getEventDate());
 
-        return  trackingDto;
+        return trackingDto;
+
+    }
+
+    private TrackingEntity convertToEntity(TrackingDTO trackingDTO) {
+        TrackingEntity tracking = new TrackingEntity();
+        tracking.setStatus(trackingDTO.getStatus());
+        tracking.setShipmentId(trackingDTO.getShipmentId());
+        tracking.setEventDate(new Date());
+
+        return tracking;
     }
 
 }
